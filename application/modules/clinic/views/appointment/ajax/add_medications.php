02bbcf7b905328a962e32b7559a584ceebea7fd8
bhishamtrehan <aman@visions.net.in>
2016-07-12 17:40:42 +0530

    changes

--- application/modules/clinic/views/appointment/ajax/add_medications.php
+++ application/modules/clinic/views/appointment/ajax/add_medications.php
@@ -1,7 +1,7 @@
 <div class="modal-header">
     <button type="button" class="close" data-dismiss="modal">&times;</button>
 	   <h4 class="modal-title">
-		<?php echo 'Add Medications'; ?>	
+		<?php echo $this->lang->line('add_medication'); ?>	
 	</h4>
 </div>
 <div class="modal-body ">
@@ -10,16 +10,16 @@
      <form class ='form-horizontal form_health form-popup-main' id = 'add_medication'>
         <div class="col-md-12 form-group">
           <div class="col-md-3">
-		  	<label for="medication">Medication</label>
+		  	<label for="medication"><?php echo $this->lang->line('medication_medication');?></label>
 			<input type="text" id="medication" class="form-control required" name="medication">
 		  </div>
 		  <div class="col-md-3">
-		  	<label for="dose">Dose</label>
+		  	<label for="dose"><?php echo $this->lang->line('medication_dose');?></label>
 			<input type="text" id="dose" class="form-control required" name="dose">
 		  </div>
 		  <div class="col-md-3 addnewmed">
-		  	<label for="route">Route</label>
-			<select title="Select" data-live-search="true" class="form-control required selectpicker bs-select-hidden" id="route" name="route"><option class="bs-title-option" value="">Select</option>
+		  	<label for="route"><?php echo $this->lang->line('medication_route');?></label>
+			<select title="Select" data-live-search="true" class="form-control required selectpicker bs-select-hidden" id="route" name="route"><option class="bs-title-option" value=""><?php echo $this->lang->line('medication_select');?></option>
 					<?php 
 									
 					foreach($frequency as $freq){ ?>
@@ -30,26 +30,26 @@
 			<a coords="route" class="addnewmedtype" href="javascript:;">Add New</a>
 		  </div>
 		  <div class="col-md-3 addnewmed">
-		  	<label for="frequency">Frequency</label>
-			<select title="Select" data-live-search="true" class="form-control required selectpicker bs-select-hidden" id="frequency" name="frequency"><option class="bs-title-option" value="">Select</option>
+		  	<label for="frequency"><?php echo $this->lang->line('medication_frequency');?></label>
+			<select title="Select" data-live-search="true" class="form-control required selectpicker bs-select-hidden" id="frequency" name="frequency"><option class="bs-title-option" value=""><?php echo $this->lang->line('medication_select');?></option>
 				<?php foreach($route as $rou){ ?>
 					<option value="<?php echo $rou['ID']; ?>"><?php echo $rou['route']; ?></option>
 				<?php } ?>
 			</select>
-			<a coords="frequency" class="addnewmedtype2" href="javascript:;">Add New</a>
+			<a coords="frequency" class="addnewmedtype2" href="javascript:;"><?php echo $this->lang->line('medication_add_new');?></a>
 		  </div>
         </div>
 		<div class="col-md-12 form-group">
           <div class="col-md-6">
-		  	<label for="notes">Notes</label>
+		  	<label for="notes"><?php echo $this->lang->line('medication_notes');?></label>
 			<input type="text" id="notes" class="form-control required" name="notes">
 		  </div>
 		  <div class="col-md-3">
-		  	<label for="startdate">Start Date</label>
+		  	<label for="startdate"><?php echo $this->lang->line('medication_start_date');?></label>
 			<input type="text" id="startdate" class="form-control required dob date-picker" name="startdate">
 		  </div>
 		  <div class="col-md-3">
-		  	<label for="enddate">End Date</label>
+		  	<label for="enddate"><?php echo $this->lang->line('medication_end_date');?></label>
 			<input type="text" id="enddate" class="form-control required dob date-picker" name="enddate">
 		  </div>
         </div>
@@ -57,18 +57,18 @@
 		<input type="hidden" value="create" name="action">
 		<input type="hidden" value="<?php echo $appId; ?>" name="appointment_id">
           <div class="col-md-5">
-		  	<label for="notes">Prescriber Name</label>
+		  	<label for="notes"><?php echo $this->lang->line('medication_prescriber_name');?></label>
 			<input type="text" id="prescriber_name" class="form-control" readonly="" name="prescriber_name" value="xbdfb">
 		  </div>
 		  <div class="col-md-5">
-		  	<label for="startdate">Prescriber Number</label>
+		  	<label for="startdate"><?php echo $this->lang->line('medication_prescriber_number');?></label>
 			<input type="text" id="prescriber_number" class="form-control" readonly="" name="prescriber_number" value="12345">
 		  </div>
         </div>
 		<div class="col-md-12 form-group">
           <div class="col-md-6">
-		  	 <button class="btn btn-default cancelmedic" type="submit">Cancel</button>
-			 <button class="btn btn-default okmedic" type="submit">Ok</button>
+		  	 <button class="btn btn-default cancelmedic" type="submit"><?php echo $this->lang->line('medication_cancel');?></button>
+			 <button class="btn btn-default okmedic" type="submit"><?php echo $this->lang->line('medication_submit');?></button>
 		  </div>
         </div>
 
