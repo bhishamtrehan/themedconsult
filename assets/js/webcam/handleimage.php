48320c3ad0dcb9b8bd9702c36b7ea9c3735c527b
Aman Sharma <aman@visions.net.in>
2016-07-15 14:32:01 +0530

    changes

--- assets/js/webcam/handleimage.php
+++ assets/js/webcam/handleimage.php
@@ -3,7 +3,7 @@
 session_start();
 $_SESSION['id']="1";
 $id=$_SESSION['id'];
-mysql_connect("localhost","root","") or die ("could not connect to data base");
+mysql_connect("localhost","themed","themedconsult@123") or die ("could not connect to data base");
 /* mysql_connect("localhost","root","Flash007") or die ("could not connect to data base");*/
 mysql_select_db("webimage") or ("couldn't connect to database");
  $name = date('YmdHis');
@@ -13,8 +13,8 @@ $file = file_put_contents( $newname, file_get_contents('php://input') );
 //print_r($file);
 
 if (!$file) {
-	print "Error occured here";
-	exit();
+    print "Error occured here";
+    exit();
 }
 else
 {
@@ -22,23 +22,23 @@ else
     $result=mysql_query($sql);
     $value=mysql_insert_id();
     $_SESSION["myvalue"]=$value;
-	
+    
 }
 $url = 'http://'.$_SERVER['HTTP_HOST'].'/themedconsult/assets/js/webcam/images/'.$name.".jpg";
 print "$url\n";
 
-mysql_connect("localhost","root","") or die ("could not connect to data base");
+mysql_connect("localhost","themed","themedconsult@123") or die ("could not connect to data base");
 /* mysql_connect("localhost","root","Flash007") or die ("could not connect to data base");
  * mysql_select_db("webimage") or ("couldn't connect to database");
  * */
-mysql_select_db("smarthealthcare") or die("couldn't connect to database");
+mysql_select_db("themedconsult") or die("couldn't connect to database");
 
 $name = date('YmdHis');
 $newname="images/".$name.".jpg";
 $file = file_put_contents( $newname, file_get_contents('php://input') );
 if (!$file) {
-	print "Error occured here";
-	exit();
+    print "Error occured here";
+    exit();
 }
 else
 {
@@ -46,7 +46,7 @@ else
     $result = mysql_query($sql);
     $value = mysql_insert_id();
     $_SESSION["myvalue"] = $value;
-	
+    
 }
 $url = 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['REQUEST_URI']) . '/' . $newname;
 print "$url\n";
